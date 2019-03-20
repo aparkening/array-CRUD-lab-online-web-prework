0a13267d4add957cf6ecbb920efb46b876b8174c
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-20 19:41:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,6 +11,12 @@ end
 # Take arguments of an array and an element and add that element to the end of the array
 def add_element_to_end_of_array(array, element)
   array = 
+  
+  
+  
+      array = ["wow", "I", "am", "really", "learning"]
+    element = "arrays!"
+  
 end
 
 def add_element_to_start_of_array(array, element)
