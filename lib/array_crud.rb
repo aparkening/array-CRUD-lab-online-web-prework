d307e3639929541292d0d632defd045a230852cc
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-20 19:44:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,8 +23,9 @@ def remove_element_from_end_of_array(array)
   array.pop
 end
 
+# Take an argument of an array and remove the first element in the array
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
